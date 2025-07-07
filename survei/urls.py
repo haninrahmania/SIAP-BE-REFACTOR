57bf7f2ce065cdf8315a9d274a6ec65bd6cab73d
haninrahmania <112334401+haninrahmania@users.noreply.github.com>
2025-07-07 19:18:46 +0700

    Update urls.py

--- survei/urls.py
+++ survei/urls.py
@@ -1,14 +1,16 @@
-from django.contrib import admin
 from django.urls import path
 from .views import *
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'survei', SurveiViewSet, basename='survei')
 
 urlpatterns=[
-    path("get-list-survei",get_list_survei),
+    path("get-list-survei/",get_list_survei),
     path("get-survei-detail/<str:id>/",get_survei_detail),
     path("add-survei/",add_survei),
     path("update-survei/<str:id>/",update_survei),
     path("delete-survei/<str:id>/",delete_survei),
-    #path('lokasi', get_location_data),
-    path('count-by-region/', get_survei_count_by_region),
-    path('count-dashboard/', get_survei_count_dashboard),
+    path('init_data/', survei_init_data, name='survei-init-data'),
+    path("upload-ktp/", upload_ktp, name="upload_ktp"),
 ]
