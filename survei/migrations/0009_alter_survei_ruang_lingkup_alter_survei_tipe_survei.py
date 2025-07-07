f57b53f3a1b805dedfc4ec62229243d08851c69e
lilacskyy <alyssa.larasati@ui.ac.id>
2025-07-07 18:12:00 +0700

    fix error turun lapangan

--- survei/migrations/0009_alter_survei_ruang_lingkup_alter_survei_tipe_survei.py
+++ survei/migrations/0009_alter_survei_ruang_lingkup_alter_survei_tipe_survei.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('survei', '0008_alter_survei_ruang_lingkup_alter_survei_tipe_survei'),
+        ('survei', '0003_alter_survei_ruang_lingkup_alter_survei_tipe_survei'),
     ]
 
     operations = [
